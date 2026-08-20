357b0653fd7fcfffd906d784b1139f30be600b14
Win Htut <whks3777@gmail.com>
2026-08-20 15:01:41 +0800

    Change the banner and write greeting and goodbye messages

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Stewie User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Stewie.java
@@ -0,0 +1,17 @@
+public class Stewie {
+    public static void main(String[] args) {
+        String banner = """
+                ███████╗ ████████╗ ███████╗ ██╗    ██╗ ██╗ ███████╗
+                ██╔════╝ ╚══██╔══╝ ██╔════╝ ██║    ██║ ██║ ██╔════╝
+                ███████╗    ██║    █████╗   ██║ █╗ ██║ ██║ █████╗
+                ╚════██║    ██║    ██╔══╝   ██║███╗██║ ██║ ██╔══╝
+                ███████║    ██║    ███████╗ ╚███╔███╔╝ ██║ ███████╗
+                ╚══════╝    ╚═╝    ╚══════╝  ╚══╝╚══╝  ╚═╝ ╚══════╝
+                """;
+        String greeting = "Hey there! I'm Stewie. \nWanna have a chat?";
+        String bye = "Bye! See you later."; 
+        System.out.println(banner);
+        System.out.println(greeting);
+        System.out.println(bye);
+    }
+}
