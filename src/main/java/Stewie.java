7d8c7caecfcba8fa9bbd4152527a3b734908b1f2
Win Htut <whks3777@gmail.com>
2026-08-20 15:29:53 +0800

    Make chatbot echo what user says and say bye and exit when the user say bye

--- src/main/java/Stewie.java
+++ src/main/java/Stewie.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Stewie {
     public static void main(String[] args) {
         String banner = """
@@ -9,9 +11,21 @@ public class Stewie {
                 ╚══════╝    ╚═╝    ╚══════╝  ╚══╝╚══╝  ╚═╝ ╚══════╝
                 """;
         String greeting = "Hey there! I'm Stewie. \nWanna have a chat?";
-        String bye = "Bye! See you later."; 
+        String bye = "Bye! See you later.";
         System.out.println(banner);
         System.out.println(greeting);
-        System.out.println(bye);
+        echoUserCommands(bye);
+    }
+
+    public static void echoUserCommands(String byeMsg) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(byeMsg);
+                break;
+            }
+            System.out.println("Stewie: " + input);
+        }
     }
 }
