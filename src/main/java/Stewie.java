aaa9af3450d0c8b539d784701c5728506004b3c5
Win Htut <whks3777@gmail.com>
2026-08-20 16:13:41 +0800

    chatbot can now accept new tasks and display them on request

--- src/main/java/Stewie.java
+++ src/main/java/Stewie.java
@@ -1,23 +1,15 @@
 import java.util.Scanner;
 
 public class Stewie {
-    public static void main(String[] args) {
-        String banner = """
-                ███████╗ ████████╗ ███████╗ ██╗    ██╗ ██╗ ███████╗
-                ██╔════╝ ╚══██╔══╝ ██╔════╝ ██║    ██║ ██║ ██╔════╝
-                ███████╗    ██║    █████╗   ██║ █╗ ██║ ██║ █████╗
-                ╚════██║    ██║    ██╔══╝   ██║███╗██║ ██║ ██╔══╝
-                ███████║    ██║    ███████╗ ╚███╔███╔╝ ██║ ███████╗
-                ╚══════╝    ╚═╝    ╚══════╝  ╚══╝╚══╝  ╚═╝ ╚══════╝
-                """;
-        String greeting = "Hey there! I'm Stewie. \nWanna have a chat?";
-        String bye = "Bye! See you later.";
-        System.out.println(banner);
-        System.out.println(greeting);
-        echoUserCommands(bye);
+    private String[] tasks;
+    private int taskCount;
+
+    public Stewie() {
+        this.tasks = new String[100];
+        this.taskCount = 0;
     }
 
-    public static void echoUserCommands(String byeMsg) {
+    public void echoUserCommands(String byeMsg) {
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String input = scanner.nextLine();
@@ -27,5 +19,50 @@ public class Stewie {
             }
             System.out.println("Stewie: " + input);
         }
+    }
+
+    public void addList(String byeMsg) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Tell me whats on your list!!");
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(byeMsg);
+                break;
+            }
+            if (input.equals("list")) {
+                if (this.taskCount == 0) {
+                    System.out.println("You have no task saved.");
+                } else {
+                    for (int i = 0; i < this.taskCount; i++) {
+                        System.out.printf("%d : %s %n", i + 1, this.tasks[i]);
+                    }
+                }
+            } else {
+                this.tasks[this.taskCount] = input;
+                this.taskCount++;
+            }
+        }
+    }
+
+    public void run() {
+        String banner = """
+                ███████╗ ████████╗ ███████╗ ██╗    ██╗ ██╗ ███████╗
+                ██╔════╝ ╚══██╔══╝ ██╔════╝ ██║    ██║ ██║ ██╔════╝
+                ███████╗    ██║    █████╗   ██║ █╗ ██║ ██║ █████╗
+                ╚════██║    ██║    ██╔══╝   ██║███╗██║ ██║ ██╔══╝
+                ███████║    ██║    ███████╗ ╚███╔███╔╝ ██║ ███████╗
+                ╚══════╝    ╚═╝    ╚══════╝  ╚══╝╚══╝  ╚═╝ ╚══════╝
+                """;
+        String greeting = "Hey there! I'm Stewie. \nWanna have a chat?";
+        String byeMsg = "Bye! See you later.";
+        System.out.println(banner);
+        System.out.println(greeting);
+        //this.echoUserCommands(byeMsg);
+        this.addList(byeMsg);
+    }
+
+    public static void main(String[] args) {
+        new Stewie().run();
     }
 }
