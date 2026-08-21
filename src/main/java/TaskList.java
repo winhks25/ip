a32a52f6242297c1900425fae1ea4cfb119c3feb
Win Htut <whks3777@gmail.com>
2026-08-21 15:31:32 +0800

    Use ArrayList for TaskList so we can store an arbitrary number of tasks

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,67 +1,64 @@
+import java.util.ArrayList;
+
 public class TaskList {
-    private Task[] tasks;
-    private int taskCount;
+    private ArrayList<Task> tasks;
 
     public TaskList() {
-        this.tasks = new Task[100];
-        this.taskCount = 0;
+        this.tasks = new ArrayList<>();
     }
 
     public void addToDo(String description) {
         Task newTask = new ToDo(description);
-        this.tasks[taskCount] = newTask;
-        this.taskCount++;
+        this.tasks.add(newTask);
 
         System.out.println("Got it! Added the following to your list.");
         System.out.println(newTask);
-        System.out.printf("Now you have %d tasks in the list. %n", this.taskCount);
+        System.out.printf("Now you have %d tasks in the list. %n", this.tasks.size());
     }
 
     public void addEvent(String description, String from, String to) {
         Task newTask = new Event(description, from, to);
-        this.tasks[taskCount] = newTask;
-        this.taskCount++;
+        this.tasks.add(newTask);
 
         System.out.println("Got it! Added the following to your list.");
         System.out.println(newTask);
-        System.out.printf("Now you have %d tasks in the list. %n", this.taskCount);
+        System.out.printf("Now you have %d tasks in the list. %n", this.tasks.size());
     }
 
     public void addDeadline(String description, String deadline) {
         Task newTask = new Deadline(description, deadline);
-        this.tasks[taskCount] = newTask;
-        this.taskCount++;
+        this.tasks.add(newTask);
 
         System.out.println("Got it! Added the following to your list.");
         System.out.println(newTask);
-        System.out.printf("Now you have %d tasks in the list. %n", this.taskCount);
+        System.out.printf("Now you have %d tasks in the list. %n", this.tasks.size());
     }
 
     public void markAsDone(int index) {
-        if (index < 0 || index >= this.taskCount || this.tasks[index] == null) {
-            System.out.printf("Task %d does not exist.\n", index + 1);
-            return;
+        try {
+            this.tasks.get(index).markAsDone();
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Please type in a valid task number in the format: mark <number>");
         }
-        this.tasks[index].markAsDone();
     }
 
     public void markAsUndone(int index) {
-        if (index < 0 || index > this.taskCount || this.tasks[index] == null) {
-            System.out.printf("Task %d does not exist.\n", index + 1);
-            return;
+        try {
+            this.tasks.get(index).markAsUndone();
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Please type in a valid task number in the format: mark <number>");
         }
-        this.tasks[index].markAsUndone();
     }
 
     public void printTaskList() {
         System.out.println("Here is your list of tasks.");
-        if (this.taskCount == 0) {
+        if (this.tasks.isEmpty()) {
             System.out.println("You have no task saved.");
             return;
         }
 
-        for (int i = 0; i < this.taskCount; i++) {
-            System.out.println(i + 1 + ". " + this.tasks[i].toString());
+        for (int i = 0; i < this.tasks.size(); i++) {
+            System.out.println(i + 1 + ". " + this.tasks.get(i).toString());
         }
     }
 }
